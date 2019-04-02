2d00276ece870d375c031c186490b29ea32da2b5
hechengyuan <cy_he@qq.com>
2019-04-02 12:38:04 +0800

    trans write_data as a simple way

--- trans.py
+++ trans.py
@@ -13,10 +13,11 @@ def get_all_name():
 
 
 def read_data(filename):
-    realname = 'data/{}'.format(filename)
+    realname = f'data/{filename}'
     data = xlrd.open_workbook(realname)
     table1 = data.sheets()[0]
     table2 = data.sheets()[1]
+    table3 =
     first_name = table1.cell_value(2,1)
     last_name = table1.cell_value(1,1)
     name = first_name + last_name
@@ -31,29 +32,19 @@ def read_data(filename):
     vo2max = table2.cell_value(11,7)
     return name, date, height, weight, age, gender, bmi, hrmax, mets, vo2max
 
-def write_worksheet(row,line,slice):
-    worksheet.write(row,line,slice[line])
-
 
 def write_data():
     workbook = xlwt.Workbook(encoding = 'utf-8')
     worksheet = workbook.add_sheet('worksheet')
     n = 0
+
     for file in get_all_name():
         slice = read_data(file)
-        worksheet.write(n,0,slice[0])
-        worksheet.write(n,1,slice[1])
-        worksheet.write(n,2,slice[2])
-        worksheet.write(n,3,slice[3])
-        worksheet.write(n,4,slice[4])
-        worksheet.write(n,5,slice[5])
-        worksheet.write(n,6,slice[6])
-        worksheet.write(n,7,slice[7])
-        worksheet.write(n,8,slice[8])
-        worksheet.write(n,9,slice[9])
+
+        for i in range(0,len(slice)):
+            worksheet.write(n,i,slice[i])
+
         n = n + 1
-
-
 
     workbook.save('all_data.xls')
 
